b343e879df319cfab6612800ec3127b56a117019
Nicolás Cabezas <cabezasn628@gmail.com>
2023-05-14 12:23:11 -0500

    feat(project completion): project Completion

---
+++ src/model/Nurse.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Nurse extends User{
+    private String speciality;
+    public Nurse(String name, String email) {
+        super(name, email);
+    }
+
+    @Override
+    public void showDataUser() {
+        System.out.println("Hospital: Cruz Verde");
+        System.out.println("Departamentos: Nutriologia, Pediatria");
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
